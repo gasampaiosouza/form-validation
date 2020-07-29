d93ec4c01a75607f5a36549093c02472591e749f
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-29 13:46:14 -0300

    :scissors: refactoring code

--- components/Form/Input/index.tsx
+++ components/Form/Input/index.tsx
@@ -3,48 +3,25 @@ import style from './style.module.scss';
 
 interface inputType {
 	placeholder: string;
-	type?: string;
-	width?: string;
-	required?: boolean;
+	change?: Function;
 }
 
-const Input: React.FC<inputType> = ({ placeholder, type, width, required }) => {
+const Input: React.FC<inputType> = ({ placeholder, change }) => {
 	const [value, setValue] = useState('');
 
-	const limitMax = (event, max: number) => {
-		const currentInput = event.target;
-		const value = currentInput.value;
-
-		let running = false;
-
-		return () => {
-			if (running) return;
-
-			running = true;
-
-			if (parseFloat(value) > max) currentInput.value = max;
-
-			running = false;
-		};
-	};
-
-	const currentYear = new Date().getFullYear();
-
 	return (
-		<div className={style.input}>
-			<input
-				className={`${style['input-box']} ${value && style['has-content']}`}
-				type={type || 'text'}
-				onChange={(e) => setValue(e.target.value)}
-				style={{ width: width || '' }}
-				required={required || true}
-				onKeyUp={(e) => limitMax(e, currentYear)()}
-				onBlur={(e) => limitMax(e, currentYear)()}
-			/>
-			<label>{placeholder}</label>
-			<span className={style['focus-border']}>
-				<i></i>
-			</span>
+		<div className={style.container}>
+			{' '}
+			<div className={style.input}>
+				<input
+					className={`${style['input-box']} ${value && style['has-content']}`}
+					onChange={(event) => (change(event), setValue(event.target.value))}
+				/>
+				<label>{placeholder}</label>
+				<span className={style['focus-border']}>
+					<i></i>
+				</span>
+			</div>
 		</div>
 	);
 };
